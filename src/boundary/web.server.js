61e94652d228be1d458515e164c94191b0a71bff
tom <twfarro@wpi.edu>
2021-05-06 22:48:15 -0400

    public folder

--- src/boundary/web.server.js
+++ src/boundary/web.server.js
@@ -30,7 +30,7 @@ async function setup(){
 
     app.post('/oauth-redirect', (req, res) => {
         console.log(req.body);
-        res.status(200).send();
+        res.status(200).send(req.body);
     })
 
 
